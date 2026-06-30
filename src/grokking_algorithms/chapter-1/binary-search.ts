9f3e891a822a4c5acf3df3fbff7bc8b3bee71ecf
TheGeniusOfEternity <max06safin@yandex.ru>
2026-06-30 21:02:37 +0300

    feat(grokking_algorithms|chapter-1): binary seach iterative impl

--- src/grokking_algorithms/chapter-1/binary-search.ts
+++ src/grokking_algorithms/chapter-1/binary-search.ts
@@ -1,5 +1,31 @@
 // Binary Search
 
+// Iterative implementation
+
+/**
+ *
+ * @param nums sorted array for search
+ * @param target searching element
+ *
+ * @returns index of a searching element, null if the element is not present
+ */
+const solution_iterative = (nums: number[], target: number) => {
+  let start = 0;
+  let end = nums.length;
+  let iterations = 0;
+
+  while (start <= end) {
+    iterations++;
+    const index = Math.floor((start + end) / 2);
+    const num = nums[index];
+    if (num === target) return [index, iterations];
+    if (target < num) end = index - 1;
+    else start = index + 1;
+  }
+
+  return [null, iterations];
+}
+
 // Recursive implementation
 
 /**
@@ -7,14 +33,14 @@
  * @param nums sorted array for search
  * @param target searching element
  *
- * @returns index of a searching element, null if element is not present
+ * @returns index of a searching element, null if the element is not present
  */
-const solution= (nums: number[], target: number) => {
+const solution_recursive= (nums: number[], target: number) => {
   let iterations = 0;
   const search = (start: number, end: number): number | null => {
     iterations++
     const index = Math.floor((start + end) / 2);
-    const num = nums[index]
+    const num = nums[index];
     if (num === target) return index;
     if (start === end) return null;
     return num > target
@@ -23,6 +49,11 @@ const solution= (nums: number[], target: number) => {
   }
   return [search(0, nums.length - 1), iterations];
 }
-const data: number[] = []
-for (let i = 0; i < 2; i++) data.push(i)
-console.log(solution([0, 1], 1))
+
+
+// Check
+const data: number[] = [];
+for (let i = 0; i < 4; i++) data.push(i);
+
+console.log(solution_iterative(data, -1));
+console.log(solution_recursive(data, -1));
