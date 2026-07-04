1266c3332e497534791eaa7b65b600a4812a3da2
TheGeniusOfEternity <max06safin@yandex.ru>
2026-07-04 16:39:23 +0300

    feat(grokking-algorithms|chapter-11): export class

--- src/grokking_algorithms/chapter-11/binary-search-tree.ts
+++ src/grokking_algorithms/chapter-11/binary-search-tree.ts
@@ -5,7 +5,7 @@
  * @left - left child
  * @right - right child
  */
-class BSTNode {
+export class BSTNode {
   value: number;
   left: BSTNode | null;
   right: BSTNode | null;
@@ -25,7 +25,7 @@ class BSTNode {
  */
 export const buildBST = (nums: number[]): BSTNode | null => {
   let root: BSTNode | null = null;
-  const addNode = (node: BSTNode | null, value: number): BSTNode | null  => {
+  const addNode = (node: BSTNode | null, value: number): BSTNode  => {
     if (node === null) return new BSTNode(value, null, null);
     if (node.value > value) node.left = addNode(node.left, value);
     else node.right = addNode(node.right, value);
