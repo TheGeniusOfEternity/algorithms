a7fb0a5113734e29f6d0674b885f065105f6a4a9
TheGeniusOfEternity <max06safin@yandex.ru>
2026-07-05 14:36:50 +0300

    feat(utils): parse num

---
+++ src/utils/parse-num.ts
@@ -0,0 +1,22 @@
+/**
+ * Parses number from input string
+ *
+ * @param idx pointer in provided input string
+ * @param input string of text
+ *
+ * @retuns nearest signed number
+ */
+export const parseNum = (idx: { val: number }, input: string): number => {
+  const len = input.length;
+  let i = idx.val
+  while (i < len && (input[i] === ' ' || input[i] === '\n' || input[i] === '\r')) i++;
+  let sign = 1;
+  if (input[i] === '-') { sign = -1; i++; }
+  let num = 0;
+  while (i < len && input[i] >= '0' && input[i] <= '9') {
+    num = num * 10 + (input.charCodeAt(i) - 48);
+    i++;
+  }
+  idx.val = i;
+  return num * sign;
+}
