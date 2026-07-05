bb79f26c131c73066fe43ce59af45c5ad1a3ffef
TheGeniusOfEternity <max06safin@yandex.ru>
2026-07-05 13:14:46 +0300

    refactor(coderun|summer-challenge|task-1): use bufferized input

--- src/coderun/summer-challenge/task-1/bicycle-rent.ts
+++ src/coderun/summer-challenge/task-1/bicycle-rent.ts
@@ -1,31 +1,41 @@
-import readline from 'readline';
+import * as fs from "node:fs";
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+const input = fs.readFileSync(0, 'utf-8');
+const len = input.length;
 
-let n: number, t: number;
-let sum: number = 0, mx: number = 0, lineNumber: number = 0;
-let used: number[];
+let sum: number = 0;
+let mx: number = 0;
+let idx: number = 0;
 
-rl.on('line', (input) => {
-  if (lineNumber === 0) {
-    [n, t] = input.split(' ').map(Number);
-    used = new Array(t + 1).fill(0);
-    lineNumber++;
-  } else {
-    const [a, f, s] = input.split(' ').map(Number);
-    used[a] += s;
-    used[f] -= s;
-    lineNumber++;
-    if (lineNumber === n + 1) {
-      rl.close();
-      for (const use of used) {
-        sum += use;
-        mx = Math.max(mx, sum);
-      }
-      console.log(mx)
-    }
+const parseNum = (): number => {
+  while (idx < len && (input[idx] === ' ' || input[idx] === '\n' || input[idx] === '\r')) idx++;
+  let sign = 1;
+  if (input[idx] === '-') { sign = -1; idx++; }
+  let num = 0;
+  while (idx < len && input[idx] >= '0' && input[idx] <= '9') {
+    num = num * 10 + (input.charCodeAt(idx) - 48);
+    idx++;
   }
-});
+  return num * sign;
+}
+
+const n = parseNum();
+const t = parseNum();
+
+let used: number[] = Array(t + 1).fill(0);
+
+for (let i = 0; i < n; i++) {
+  const a = parseNum();
+  const f = parseNum();
+  const s = parseNum();
+
+  used[a] += s;
+  used[f] -= s;
+}
+
+for (let i = 0; i < used.length; i++) {
+  sum += used[i];
+  mx = Math.max(mx, sum);
+}
+
+console.log(mx);
