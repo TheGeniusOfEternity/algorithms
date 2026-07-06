8e9b7abf19608fa3c27aa294ae113f2e5fd5c410
TheGeniusOfEternity <max06safin@yandex.ru>
2026-07-06 21:03:24 +0300

    fix(leetcode|30-days-of-js|task-1): supress unused var warning

--- src/leetcode/30-days-of-ts/create-hello-world-function.ts
+++ src/leetcode/30-days-of-ts/create-hello-world-function.ts
@@ -1,6 +1,6 @@
 /**
  * @retuns "Hello World" after call with any arguments
  */
-export function createHelloWorld(...args: any[]) {
-  return (...args: any[]) => "Hello World";
+export function createHelloWorld(..._args: any[]) {
+  return (..._args: any[]) => "Hello World";
 }
