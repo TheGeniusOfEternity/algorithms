716c94f35f39cc4ec9b746aead40d383a369729c
TheGeniusOfEternity <max06safin@yandex.ru>
2026-07-09 15:43:21 +0300

    refactor(coderun|summer-challenge|task-6): deque solution 35/35

--- src/coderun/summer-challenge/task-6/koderun-covers-the-beach.ts
+++ src/coderun/summer-challenge/task-6/koderun-covers-the-beach.ts
@@ -1,5 +1,6 @@
 import fs from "node:fs";
 import { parseNum } from "../../../utils/parse-num";
+import { parseBigInt } from "../../../utils/parse-bigint";
 
 const input = fs.readFileSync(0, 'utf-8');
 const idx = { val: 0 };
@@ -9,35 +10,50 @@ let output = "";
 for (let i = 0; i < t; i++) {
   const n = parseNum(idx, input);
   const k = parseNum(idx, input);
-  const pref_sum: number[] = [0];
-  const pref_xor: number[] = [0];
+  const pref_sum = new BigInt64Array(n + 1);
+  const pref_xor = new BigInt64Array(n + 1);
 
   for (let j = 0; j < n; j++) {
-    const num = parseNum(idx, input);
+    const num = parseBigInt(idx, input);
     pref_sum[j + 1] = pref_sum[j] + num;
     pref_xor[j + 1] = pref_xor[j] ^ num;
   }
 
-  for (let j = 0; j < n; j++) {
-    let min = Infinity;
-    for (let l = 0; l <= j; l++) {
-      for (let r = j; r < n; r++) {
-        const length = r - l + 1;
-        if (length >= k) {
-          const s = pref_sum[r + 1] - pref_sum[l];
-          const x = pref_xor[r + 1] ^ pref_xor[l];
-          const f = s - x;
-          min = min > f ? f : min;
-        }
+  const V_len = n - k + 1;
+  const V = new BigInt64Array(V_len);
+  for (let l = 0; l < V_len; l++) {
+    const r = l + k - 1;
+    const s = pref_sum[r + 1] - pref_sum[l];
+    const x = pref_xor[r + 1] ^ pref_xor[l];
+    V[l] = s - x;
+  }
+
+  const deque = new Int32Array(V_len + 1);
+  let head = 0, tail = 0;
+  let current_R = -1;
+
+  for (let i = 0; i < n; i++) {
+    const L = Math.max(0, i - k + 1);
+    const R = Math.min(i, n - k);
+
+    while (current_R < R) {
+      current_R++;
+      while (tail > head && V[deque[tail - 1]] >= V[current_R]) {
+        tail--;
       }
+      deque[tail++] = current_R;
     }
-    output += min + " ";
+    while (head < tail && deque[head] < L) {
+      head++;
+    }
+    
+    output += V[deque[head]].toString() + " ";
     if (output.length > 65536) {
       process.stdout.write(output);
       output = "";
     }
   }
-  output += "\n"
+  output += "\n";
   if (output.length > 65536) {
     process.stdout.write(output);
     output = "";
