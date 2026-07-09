9acb22b6b401e8185e069e9329de061c44e36288
TheGeniusOfEternity <max06safin@yandex.ru>
2026-07-09 16:10:23 +0300

    feat(leetcode|30-days-of-js|task-6): solution

---
+++ src/leetcode/30-days-of-ts/task-6/filter-elements-from-array.ts
@@ -0,0 +1,14 @@
+/**
+ *
+ * @param arr - array of elements
+ * @param fn - comparing function
+ *
+ * @returns new array that contains the elements from the arr for which the expression fn(arr[i], i) evaluates to a truthy value (Boolean(value) returns true)
+ */
+export const filter = (arr: number[], fn: (n: number, i: number) => any) => {
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i], i)) result.push(arr[i]);
+  }
+  return result;
+}
