dfa63c2c708b975d6cda151afbfe6f1ee60e4d72
TheGeniusOfEternity <max06safin@yandex.ru>
2026-07-09 16:10:15 +0300

    test(leetcode|30-days-of-js|task-6): example testcases

---
+++ src/leetcode/30-days-of-ts/task-6/filter-elements-from-array.test.ts
@@ -0,0 +1,27 @@
+import { filter } from "./filter-elements-from-array";
+
+describe('Task #6 | Filter Elements From Array | Example testcases', () => {
+  test('#1 Greater Than 10', () => {
+    const input = [0, 10, 20, 30];
+    const fn = (n: number) => n > 10;
+    const expected = [20, 30];
+    const output = filter(input, fn)
+    expect(output).toEqual(expected);
+  })
+
+  test('#2 First Index', () => {
+    const input = [1, 2, 3];
+    const fn = (_: number, i: number) => i === 0;
+    const expected = [1];
+    const output = filter(input, fn)
+    expect(output).toEqual(expected);
+  })
+
+  test('#3 Plus one', () => {
+    const input = [-2, -1, 0, 1, 2];
+    const fn = (n: number) => ++n;
+    const expected = [-2, 0, 1, 2];
+    const output = filter(input, fn)
+    expect(output).toEqual(expected);
+  })
+})
