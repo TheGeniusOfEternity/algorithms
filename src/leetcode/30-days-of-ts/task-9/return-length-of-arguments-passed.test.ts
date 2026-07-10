5bbb1a7162d78761e4a18761810eb0de0e6315a1
TheGeniusOfEternity <max06safin@yandex.ru>
2026-07-10 14:20:56 +0300

    test(leetcode|30-days-of-js|task-9): example testcases

---
+++ src/leetcode/30-days-of-ts/task-9/return-length-of-arguments-passed.test.ts
@@ -0,0 +1,17 @@
+import { argumentsLength } from "./return-length-of-arguments-passed";
+
+describe('Task #9 | Return Length of Arguments Passed | Example Testcases', () => {
+  test('#1 Single value', () => {
+    const input = [1];
+    const expected = 1;
+    const output = argumentsLength(...input);
+    expect(output).toBe(expected);
+  })
+
+  test('#2 Multiple values with different types', () => {
+    const input = [{}, null, "3"];
+    const expected = 3;
+    const output = argumentsLength(...input);
+    expect(output).toBe(expected);
+  })
+})
