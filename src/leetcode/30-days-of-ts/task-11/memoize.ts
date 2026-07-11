76abce12a0f55377d6ff1e437d919cf335930c64
TheGeniusOfEternity <max06safin@yandex.ru>
2026-07-11 15:41:48 +0300

    feat(leetcode|30-days-of-js|task-11): solution

---
+++ src/leetcode/30-days-of-ts/task-11/memoize.ts
@@ -0,0 +1,23 @@
+export type Fn = (...params: number[]) => number
+
+/**
+ *
+ * @param fn - function to memoized
+ *
+ * @returns memoized fn - function will be called with unique arg only single time, then result value from cached will be returned
+ */
+export const memoize = (fn: Fn): Fn => {
+  const cache = new Map<string, number>();
+
+  return (...params: number[]): number => {
+    const key = JSON.stringify(params);
+    const cached = cache.get(key);
+
+    if (cached !== undefined) return cached;
+
+    const value = fn(...params);
+    cache.set(key, value);
+
+    return value;
+  }
+}
