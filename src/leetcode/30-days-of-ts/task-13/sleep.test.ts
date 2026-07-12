f0cfef5f1d358595ccb7212b2141dd65c3b4517d
TheGeniusOfEternity <max06safin@yandex.ru>
2026-07-12 21:29:20 +0300

    fix(leetcode|30-days-of-js|task-13): expect outside promise

--- src/leetcode/30-days-of-ts/task-13/sleep.test.ts
+++ src/leetcode/30-days-of-ts/task-13/sleep.test.ts
@@ -1,23 +1,19 @@
 import { sleep } from "./sleep";
 
 describe('Task #13 | Sleep | Example testcases', () => {
-  test('#1 100 Millis', () => {
+  test('#1 100 Millis', async () => {
     const input = 100;
     const expected = 100;
     const t = Date.now();
-    sleep(input).then(() => {
-      const output = Date.now() - t;
-      expect(output).toBe(expected);
-    })
+    const output = await sleep(input).then(() => Date.now() - t);
+    expect(output).toBeCloseTo(expected, -1);
   });
 
-  test('#2 200 Millis', () => {
+  test('#2 200 Millis', async () => {
     const input = 200;
     const expected = 200;
     const t = Date.now();
-    sleep(input).then(() => {
-      const output = Date.now() - t;
-      expect(output).toBe(expected);
-    })
+    const output = await sleep(input).then(() => Date.now() - t);
+    expect(output).toBeCloseTo(expected, -1);
   });
 });
