076a9c11145afcc779cf47d7d6a91b6fb8b6d01c
TheGeniusOfEternity <max06safin@yandex.ru>
2026-07-17 20:48:02 +0300

    feat(leetcode|30-days-of-js|task-18): solution

---
+++ src/leetcode/30-days-of-ts/promises-and-time/task-18/debounce.ts
@@ -0,0 +1,16 @@
+type F = (...args: number[]) => void
+
+/**
+ *
+ * @param fn - function to debounce
+ * @param t - delay in milliseconds before execution
+ *
+ * @returns function whose execution is delayed by `t` milliseconds and whose execution is cancelled if it is called again within that window of time
+ */
+export const debounce = (fn: F, t: number): F => {
+  let lastTimerId: NodeJS.Timeout;
+  return (...args) => {
+    clearTimeout(lastTimerId);
+    lastTimerId = setTimeout(() => fn(...args), t);
+  }
+}
