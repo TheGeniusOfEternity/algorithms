0331dc51ceb7c4801c7b9d6345396a944f5c3c0c
TheGeniusOfEternity <max06safin@yandex.ru>
2026-07-20 23:04:41 +0300

    feat(leetcode|30-days-of-js|task-22): solution

---
+++ src/leetcode/30-days-of-ts/json/task-22/array-prototype-last.ts
@@ -0,0 +1,15 @@
+export {};
+
+declare global {
+  interface Array<T> {
+    last(): T | -1;
+  }
+}
+
+/**
+ * @returns The last element of the array. If there are no elements in the array, it `-1` will be returned.
+ */
+Array.prototype.last = function<T>(): T | -1 {
+  const length = this.length;
+  return length === 0 ? -1 : this[length - 1];
+};
