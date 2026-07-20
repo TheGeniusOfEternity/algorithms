a7f283b94faace17ddf54a198f02a6eaf6a64795
TheGeniusOfEternity <max06safin@yandex.ru>
2026-07-20 23:04:33 +0300

    test(leetcode|30-days-of-js|task-22): testcases

---
+++ src/leetcode/30-days-of-ts/json/task-22/array-prototype-last.test.ts
@@ -0,0 +1,17 @@
+import './array-prototype-last';
+
+describe('Task #22 | Array Prototype Last | Testcases', () => {
+  test('#1 Non-empty Array', () => {
+    const arr = [null, {}, 3];
+    const expected = 3;
+    const output = arr.last();
+    expect(output).toEqual(expected);
+  })
+
+  test('#2 Empty Array', () => {
+    const arr: number[] = [];
+    const expected = -1;
+    const output = arr.last();
+    expect(output).toEqual(expected);
+  })
+})
