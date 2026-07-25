4e04351aaf05da399652e64d557a6f52b4d26e0a
TheGeniusOfEternity <max06safin@yandex.ru>
2026-07-25 12:56:45 +0300

    feat(neetcode|roadmap|product-of-array-except-self): division solution

---
+++ src/neetcode/arrays-&-hashing/product-of-array-except-self/solution.ts
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums - an integer array
+ * @return {number[]} - an array `output` where `output[i]` is the product of all the elements of `nums` except `nums[i]`
+ */
+export const productExceptSelf = (nums: number[]): number[] => {
+  const products = Array.from<number>({ length: nums.length }).fill(0);
+  let product = 1;
+  let zeros = 0;
+
+  for (const num of nums) {
+    if (num === 0) {
+      zeros++;
+    } else {
+      product *= num;
+    }
+  }
+
+  if (zeros > 1) {
+    return products;
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    products[i] = zeros === 1 ? (nums[i] === 0 ? product : 0) : product / nums[i];
+  }
+
+  return products;
+};
