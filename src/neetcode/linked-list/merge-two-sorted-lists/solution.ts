186c7845ce9e13bb73043710557fed05fe34501a
TheGeniusOfEternity <max06safin@yandex.ru>
2026-08-15 16:13:39 +0300

    feat(neetcode|roadmap|merge-two-sorted-lists): solution

---
+++ src/neetcode/linked-list/merge-two-sorted-lists/solution.ts
@@ -0,0 +1,33 @@
+import { ListNode } from '../common';
+
+/**
+ * @param {ListNode} list1 - sorted linked list
+ * @param {ListNode} list2 - sorted linked list
+ * @return {ListNode} - the head of the new **sorted** linked list.
+ * The list should be made up of nodes from `list1` and `list2`.
+ */
+export const mergeTwoLists = (
+  list1: ListNode | null,
+  list2: ListNode | null,
+): ListNode | null => {
+  const dummy = new ListNode();
+  let curr = dummy;
+  while (list1 !== null && list2 !== null) {
+    if (list1.val < list2.val) {
+      curr.next = list1;
+      list1 = list1.next;
+    } else {
+      curr.next = list2;
+      list2 = list2.next;
+    }
+    curr = curr.next;
+  }
+
+  if (list1) {
+    curr.next = list1;
+  } else if (list2) {
+    curr.next = list2;
+  }
+
+  return dummy.next;
+};
