26fdf7b44a8d5a9b2946273db72e618479dcd436
TheGeniusOfEternity <max06safin@yandex.ru>
2026-08-21 18:25:38 +0300

    feat(interview|build-tree|no-replies-sort): solution

---
+++ src/interview/build-tree/no-replies-sort/solution.ts
@@ -0,0 +1,62 @@
+interface Message {
+  message: string;
+  pass: string;
+}
+
+interface NestedMessage {
+  message: string;
+  replies?: NestedMessage[];
+}
+
+/**
+ * @param messages - arrays of messages
+ * @returns array of nested messages
+ * @example
+ *
+ * const messages = [
+ *   { Message: 'Deep', Pass: '1.10.5' },
+ *   { Message: 'Root', Pass: '1' },
+ *   { Message: 'Child 10', Pass: '1.10' },
+ *   { Message: 'Child 2', Pass: '1.2' },
+ * ];
+ *
+ * console.log(JSON.stringify(buildTree(messages), null, 2));
+ *
+ * // Expected
+ * // Root
+ * // ├─ Child 10
+ * // │  └─ Deep
+ * // └─ Child 2
+ */
+export const buildTree = (messages: Message[]): NestedMessage[] => {
+  const map = new Map<
+    string,
+    { copy: NestedMessage; parentPath: string | null }
+  >();
+  const roots: NestedMessage[] = [];
+  for (const message of messages) {
+    const parentPathIdx = message.pass.lastIndexOf('.');
+    const copy = { message: message.message };
+    map.set(message.pass, {
+      copy,
+      parentPath:
+        parentPathIdx === -1 ? null : message.pass.slice(0, parentPathIdx),
+    });
+  }
+
+  for (const { copy, parentPath } of map.values()) {
+    if (parentPath === null) {
+      roots.push(copy);
+      continue;
+    }
+    const parent = map.get(parentPath)?.copy;
+    if (!parent) {
+      roots.push(copy);
+      continue;
+    }
+    parent.replies ??= [];
+    parent.replies.push(copy);
+  }
+
+  return roots;
+};
